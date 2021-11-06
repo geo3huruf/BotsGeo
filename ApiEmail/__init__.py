b8c55daa0124ba5224d75098f2e7c048cc99900a
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2021-11-07 01:19:04 +0700

    Add files via upload

---
+++ ApiEmail/__init__.py
@@ -0,0 +1,16 @@
+#from .client import LINE
+from .channel import Channel
+from .ttypes import OpType
+
+__modified__        = 'Denmas Geo USA_cretions'
+__copyright__       = 'Copyright 2021 by denmas geo'
+__version__         = '3.0.8'
+__license__         = 'BSD-3-Clause'
+__author__          = 'Denmas Geo'
+__author_email__    = 'denmasgeo05@gmail.com'
+
+
+
+
+
+__all__ = ['LINE', 'Channel','OpType']
